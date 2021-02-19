ba4918057b5c5b30d7a26fe603862de37aafa8ab
Eryk Franke <erykfranke98@gmail.com>
2021-02-19 03:36:03 +0100

    change global_api_url to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     heatmap_api_url: 'https://cryptic-springs-76768.herokuapp.com/',
-    global_api_url: 'http://server437671.nazwa.pl',
+    global_api_url: 'https://server437671.nazwa.pl',
 };
