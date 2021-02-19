7163349a55b159a5ae9c72018c3d4c379d311347
Eryk Franke <erykfranke98@gmail.com>
2021-02-19 03:47:58 +0100

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     heatmap_api_url: 'https://cryptic-springs-76768.herokuapp.com/',
-    global_api_url: 'https://server437671.nazwa.pl',
+    global_api_url: 'https://dev.grabitstudio.com/heat.php',
 };
